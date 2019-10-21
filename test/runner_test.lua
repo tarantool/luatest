25064dc2ea7924b1630a70219c716f4a7fa24feb
Max Melentiev <m.melentiev@corp.mail.ru>
2019-10-21 12:46:39 +0300

    Make --shuffle option accept `group`, `all`, `none` values

--- test/runner_test.lua
+++ test/runner_test.lua
@@ -42,3 +42,52 @@ end
 g.test_executable_error = function()
     t.assert_equals(run_file('error.lua'), 256) -- luajit multiplies result by 256
 end
+
+g.test_run_shuffle = function()
+    local function get_run_order(options)
+        local result = {}
+        helper.run_suite(function(lu2)
+            for i = 1, 3 do
+                local g2 = lu2.group('g' .. i)
+                g2.test_1 = function() table.insert(result, '' .. i .. '-a') end
+                g2.test_2 = function() table.insert(result, '' .. i .. '-b') end
+            end
+        end, options)
+        return result
+    end
+
+    t.assert_equals(get_run_order({'--shuffle', 'none'}), {
+        '1-a',
+        '1-b',
+        '2-a',
+        '2-b',
+        '3-a',
+        '3-b',
+    })
+
+    t.assert_equals(get_run_order({'--shuffle', 'group:1'}), {
+        '1-a',
+        '1-b',
+        '2-a',
+        '2-b',
+        '3-b',
+        '3-a',
+    })
+    t.assert_equals(get_run_order({'--shuffle', 'group:2'}), {
+        '1-a',
+        '1-b',
+        '2-b',
+        '2-a',
+        '3-b',
+        '3-a',
+    })
+
+    t.assert_equals(get_run_order({'--shuffle', 'all:1'}), {
+        '3-a',
+        '2-b',
+        '3-b',
+        '2-a',
+        '1-b',
+        '1-a',
+    })
+end
