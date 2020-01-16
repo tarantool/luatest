01aaa8792aa94bb7321508c23278f52008a88730
Max Melentiev <m.melentiev@corp.mail.ru>
2020-01-16 17:49:08 +0300

    Patch luacov

--- luatest/coverage_utils.lua
+++ luatest/coverage_utils.lua
@@ -1,5 +1,16 @@
 local runner = require('luacov.runner')
 
+-- Fix luacov issue. Without patch it's failed when `assert` is redefined.
+function runner.update_stats(old_stats, extra_stats)
+   old_stats.max = math.max(old_stats.max, extra_stats.max)
+   for line_nr, run_nr in pairs(extra_stats) do
+      if type(line_nr) == 'number' then -- This line added instead of `= nil`
+        old_stats[line_nr] = (old_stats[line_nr] or 0) + run_nr
+        old_stats.max_hits = math.max(old_stats.max_hits, old_stats[line_nr])
+      end
+   end
+end
+
 -- Module with utilities for collecting code coverage from external processes.
 local export = {
     DEFAULT_EXCLUDE = {
