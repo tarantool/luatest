3d6befd352b1bbc3d37180d71c5470fd4283ec77
Max Melentiev <m.melentiev@corp.mail.ru>
2020-03-03 19:32:02 +0300

    Extract sorted_pairs from luaunit

--- luatest/luaunit.lua
+++ luatest/luaunit.lua
@@ -3,6 +3,7 @@ require("math")
 
 local Class = require('luatest.class')
 local comparator = require('luatest.comparator')
+local sorted_pairs = require('luatest.sorted_pairs')
 
 local M={}
 
@@ -117,99 +118,6 @@ Options:
 local function os_exit(code)
     error({type = 'LUAUNIT_EXIT', code = code})
 end
-
-local crossTypeOrdering = {
-    number = 1, boolean = 2, string = 3, table = 4, other = 5
-}
-local crossTypeComparison = {
-    number = function(a, b) return a < b end,
-    string = function(a, b) return a < b end,
-    other = function(a, b) return tostring(a) < tostring(b) end,
-}
-
-local function cross_type_sort(a, b)
-    local type_a, type_b = type(a), type(b)
-    if type_a == type_b then
-        local func = crossTypeComparison[type_a] or crossTypeComparison.other
-        return func(a, b)
-    end
-    type_a = crossTypeOrdering[type_a] or crossTypeOrdering.other
-    type_b = crossTypeOrdering[type_b] or crossTypeOrdering.other
-    return type_a < type_b
-end
-
-local function __gen_sorted_index( t )
-    -- Returns a sequence consisting of t's keys, sorted.
-    local sortedIndex = {}
-
-    for key,_ in pairs(t) do
-        table.insert(sortedIndex, key)
-    end
-
-    table.sort(sortedIndex, cross_type_sort)
-    return sortedIndex
-end
-M.private.__gen_sorted_index = __gen_sorted_index
-
-local function sorted_next(state, control)
-    -- Equivalent of the next() function of table iteration, but returns the
-    -- keys in sorted order (see __gen_sorted_index and cross_type_sort).
-    -- The state is a temporary variable during iteration and contains the
-    -- sorted key table (state.sortedIdx). It also stores the last index (into
-    -- the keys) used by the iteration, to find the next one quickly.
-    local key
-
-    --print("sorted_next: control = "..tostring(control) )
-    if control == nil then
-        -- start of iteration
-        state.count = #state.sortedIdx
-        state.lastIdx = 1
-        key = state.sortedIdx[1]
-        return key, state.t[key]
-    end
-
-    -- normally, we expect the control variable to match the last key used
-    if control ~= state.sortedIdx[state.lastIdx] then
-        -- strange, we have to find the next value by ourselves
-        -- the key table is sorted in cross_type_sort() order! -> use bisection
-        local lower, upper = 1, state.count
-        repeat
-            state.lastIdx = math.modf((lower + upper) / 2)
-            key = state.sortedIdx[state.lastIdx]
-            if key == control then
-                break -- key found (and thus prev index)
-            end
-            if cross_type_sort(key, control) then
-                -- key < control, continue search "right" (towards upper bound)
-                lower = state.lastIdx + 1
-            else
-                -- key > control, continue search "left" (towards lower bound)
-                upper = state.lastIdx - 1
-            end
-        until lower > upper
-        if lower > upper then -- only true if the key wasn't found, ...
-            state.lastIdx = state.count -- ... so ensure no match in code below
-        end
-    end
-
-    -- proceed by retrieving the next value (or nil) from the sorted keys
-    state.lastIdx = state.lastIdx + 1
-    key = state.sortedIdx[state.lastIdx]
-    if key then
-        return key, state.t[key]
-    end
-
-    -- getting here means returning `nil`, which will end the iteration
-end
-
-local function sorted_pairs(tbl)
-    -- Equivalent of the pairs() function on tables. Allows to iterate in
-    -- sorted order. As required by "generic for" loops, this will return the
-    -- iterator (function), an "invariant state", and the initial control value.
-    -- (see http://www.lua.org/pil/7.2.html)
-    return sorted_next, {t = tbl, sortedIdx = __gen_sorted_index(tbl)}, nil
-end
-M.private.sorted_pairs = sorted_pairs
 
 local function randomize_table( t )
     -- randomize the item orders of the table t
